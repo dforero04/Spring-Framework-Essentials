e90c4181cf02d4d4581950ec000c93916f1db47c
Daniel Forero <dforero@vmware.com>
2023-03-22 11:01:35 -0500

    WIP: JdbcTemplate lab

--- 26-jdbc/src/main/java/rewards/internal/reward/JdbcRewardRepository.java
+++ 26-jdbc/src/main/java/rewards/internal/reward/JdbcRewardRepository.java
@@ -1,6 +1,7 @@
 package rewards.internal.reward;
 
 import common.datetime.SimpleDate;
+import org.springframework.jdbc.core.JdbcTemplate;
 import rewards.AccountContribution;
 import rewards.Dining;
 import rewards.RewardConfirmation;
@@ -36,20 +37,25 @@ import java.sql.*;
 
 public class JdbcRewardRepository implements RewardRepository {
 
-	private DataSource dataSource;
+	private final DataSource dataSource;
+
+	private final JdbcTemplate jdbcTemplate;
 
 	public JdbcRewardRepository(DataSource dataSource) {
 		this.dataSource = dataSource;
+		jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 
 	public RewardConfirmation confirmReward(AccountContribution contribution, Dining dining) {
 		String sql = "insert into T_REWARD (CONFIRMATION_NUMBER, REWARD_AMOUNT, REWARD_DATE, ACCOUNT_NUMBER, DINING_MERCHANT_NUMBER, DINING_DATE, DINING_AMOUNT) values (?, ?, ?, ?, ?, ?, ?)";
 		String confirmationNumber = nextConfirmationNumber();
 
+		jdbcTemplate.update(sql, confirmationNumber, contribution.getAmount().asBigDecimal(), new Date(SimpleDate.today().inMilliseconds()), contribution.getAccountNumber(), dining.getMerchantNumber(), new Date(dining.getDate().inMilliseconds()), dining.getAmount().asBigDecimal());
+
 		// Update the T_REWARD table with the new Reward
 		try (Connection conn = dataSource.getConnection();
 			 PreparedStatement ps = conn.prepareStatement(sql)) {
-			
+
 			ps.setString(1, confirmationNumber);
 			ps.setBigDecimal(2, contribution.getAmount().asBigDecimal());
 			ps.setDate(3, new Date(SimpleDate.today().inMilliseconds()));
@@ -61,15 +67,15 @@ public class JdbcRewardRepository implements RewardRepository {
 		} catch (SQLException e) {
 			throw new RuntimeException("SQL exception occurred inserting reward record", e);
 		}
-		
+
 		return new RewardConfirmation(confirmationNumber, contribution);
 	}
 
 	private String nextConfirmationNumber() {
 		String sql = "select next value for S_REWARD_CONFIRMATION_NUMBER from DUAL_REWARD_CONFIRMATION_NUMBER";
 		String nextValue;
-		
-		try (Connection conn = dataSource.getConnection(); 
+
+		try (Connection conn = dataSource.getConnection();
 			 PreparedStatement ps = conn.prepareStatement(sql);
 			 ResultSet rs = ps.executeQuery()) {
 			rs.next();
@@ -77,7 +83,7 @@ public class JdbcRewardRepository implements RewardRepository {
 		} catch (SQLException e) {
 			throw new RuntimeException("SQL exception getting next confirmation number", e);
 		}
-		
+
 		return nextValue;
 	}
 }
--- 26-jdbc/src/test/java/rewards/internal/reward/JdbcRewardRepositoryTests.java
+++ 26-jdbc/src/test/java/rewards/internal/reward/JdbcRewardRepositoryTests.java
@@ -74,8 +74,9 @@ public class JdbcRewardRepositoryTests {
 		//	  (If you are using Gradle, comment out the test exclude in
 		//    the build.gradle file.)
 		//
-		
-		Map<String, Object> values = null;
+		String sql = "SELECT * FROM T_REWARD WHERE CONFIRMATION_NUMBER = ?";
+		String id = confirmation.getConfirmationNumber();
+		Map<String, Object> values = jdbcTemplate.queryForMap(sql, id);
 		verifyInsertedValues(confirmation, dining, values);
 	}
 
@@ -92,7 +93,8 @@ public class JdbcRewardRepositoryTests {
 	private int getRewardCount() throws SQLException {
 		// TODO-01: Use JdbcTemplate to query for the number of rows in the T_REWARD table
 		// - Use "SELECT count(*) FROM T_REWARD" as SQL statement
-		return -1;
+		String sql = "SELECT count(*) FROM T_REWARD";
+		return jdbcTemplate.queryForObject(sql, Integer.class);
 	}
 
 	private DataSource createTestDataSource() {
